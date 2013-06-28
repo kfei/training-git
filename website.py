c0178717686d864cab83be6b83dca862f7139d43
kfei <kfei@kfei.net>
2013-06-28 11:35:53 +0800

    Add the command-line parsing module argparse
    Now the website can show a help massage with its usage.

--- website.py
+++ website.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 import sys
+import argparse
 
 class Website:
-
     def __init__(self):
         pass
 
@@ -11,5 +11,10 @@ class Website:
         print 'Hello world!'
 
 if __name__ == '__main__':
+    # Parsing arguments.
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    # Create a website and show its contents to clients.
     w = Website()
     w.index()
