115a355582d7cb3ac670f0577bc94131513ae458
kfei <kfei@kfei.net>
2013-06-28 11:48:03 +0800

    Make home page speak different words to each user
    Add a command-line argument 'username'.

--- website.py
+++ website.py
@@ -7,14 +7,15 @@ class Website:
     def __init__(self):
         pass
 
-    def index(self):
-        print 'Hello world!'
+    def home(self, username):
+        print 'Hello %s!' % username
 
 if __name__ == '__main__':
     # Parsing arguments.
     parser = argparse.ArgumentParser()
+    parser.add_argument("username", help="the username used to log in to the website")
     args = parser.parse_args()
 
     # Create a website and show its contents to clients.
     w = Website()
-    w.index()
+    w.home(args.username)
