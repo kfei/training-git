e78130ad906de33cb20b597070bd9be744c21425
kfei <kfei@kfei.net>
2014-05-21 15:50:26 +0800

    Bump version to 0.2

--- args.py
+++ args.py
@@ -5,5 +5,5 @@ import argparse
 def get_args():
     p = argparse.ArgumentParser()
     p.add_argument("username",
-                    help="the username used to visit the website")
+                   help="the username used to visit the website")
     return p.parse_args()
