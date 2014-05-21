069008ccc8610e39926cd3af928306c20e83977b
kfei <kfei@kfei.net>
2014-05-21 15:24:21 +0800

    Move username to Website's instance variable
    Instead of passing the 'username' parameter to Website's methods, move
    it to a instance variable.

--- website.py
+++ website.py
@@ -4,18 +4,18 @@ import sys
 from args import get_args
 
 class Website:
-    def __init__(self):
-        pass
+    def __init__(self, username):
+        self.username = username
 
-    def home(self, username):
-        print 'Hello %s!' % username
+    def home(self):
+        print 'Hello %s!' % self.username
 
 if __name__ == '__main__':
     # Parsing arguments.
     args = get_args()
 
     # Create a website and show its contents to clients.
-    w = Website()
+    w = Website(args.username)
 
     # Show home page to clients.
-    w.home(args.username)
+    w.home()
